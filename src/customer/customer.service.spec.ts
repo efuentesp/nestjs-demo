0cab9047e9a675bb3ffce4a4e717d499feb1cb54
efuentes <efuentes@softtek.com>
2020-12-20 13:26:05 -0600

    First Jest test passing.

--- src/customer/customer.service.spec.ts
+++ src/customer/customer.service.spec.ts
@@ -1,18 +1,21 @@
 import { MongooseModule } from '@nestjs/mongoose';
 import { Test } from '@nestjs/testing';
+import { Model } from 'mongoose';
 import {
   closeInMongodConnection,
   rootMongooseTestModule,
 } from '../test-utils/mongo/mongooseTestModule';
+import { CustomerModule } from './customer.module';
 import { CustomerService } from './customer.service';
-import { Customer, CustomerSchema } from './schemas/customer.schema';
-
-const mockCustomerModel = () => ({
-  findAll: jest.fn(),
-});
+import {
+  Customer,
+  CustomerDocument,
+  CustomerSchema,
+} from './schemas/customer.schema';
 
 describe('CustomerService', () => {
   let customerService: CustomerService;
+  let customerModel: Model<CustomerDocument>;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
@@ -26,19 +29,32 @@ describe('CustomerService', () => {
     }).compile();
 
     customerService = module.get<CustomerService>(CustomerService);
+    customerModel = module.get<Model<CustomerDocument>>('CustomerModel');
   });
 
   it('should be defined', () => {
     expect(customerService).toBeDefined();
   });
 
-  // describe('findAll Customers', async () => {
-  //   customerModel.find.mockResolvedValue('customer');
-  //   expect(customerModel.findAll).not.toHaveBeenCalled();
-  //   const result = customerService.findAll();
-  //   expect(customerModel.findAll).toHaveBeenCalled();
-  //   expect(result).toEqual('customer');
-  // });
+  describe('findAll', () => {
+    it('get all Customer items', async () => {
+      // arrange
+      await customerModel.create({
+        code: 'C001',
+        name: 'Customer 01',
+      });
+
+      // act
+      const result: Customer[] = await customerService.findAll();
+
+      // assert
+      expect(result.length).toEqual(1);
+    });
+  });
+
+  afterEach(async () => {
+    await customerModel.deleteMany({});
+  });
 
   afterAll(async () => {
     await closeInMongodConnection();
